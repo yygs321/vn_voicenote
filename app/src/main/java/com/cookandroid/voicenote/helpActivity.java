59458de2177869f3488dd0239014bf1664e6ffa2
heroku test <wjstngus1031@gmail.com>
2021-08-18 22:56:07 +0900

    도움말 추가

---
+++ app/src/main/java/com/cookandroid/voicenote/helpActivity.java
@@ -0,0 +1,45 @@
+package com.cookandroid.voicenote;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class helpActivity extends Activity {
+
+    TextView txtText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.help);
+
+        //UI 객체생성
+        txtText = (TextView)findViewById(R.id.txtText);
+
+        //데이터 가져오기
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        txtText.setText(data);
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+
+
+}
